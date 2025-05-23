b05afa9f4b31e809c6f38eb8e4d27ee7b8831125
ashhhyy <111415268+ashhhyy@users.noreply.github.com>
2025-05-23 23:29:04 +0800

    Delete esp32_firmware directory

--- esp32_firmware/ml_inference.h
+++
@@ -1,7 +0,0 @@
-#ifndef ML_INFERENCE_H
-#define ML_INFERENCE_H
-
-void initMLModel();
-bool runInference(const uint8_t *image_data, size_t length);
-
-#endif
--- esp32_firmware/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#include <esp_camera.h>
-
-void saveImageToSD(camera_fb_t * fb);
-
-#endif
